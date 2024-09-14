9e8d3d59622099ee7b6f73cc4ec375750514d759
Gleb <Glebsd22@gmail.com>
2024-09-14 18:09:41 +0300

    completed 1.2

---
+++ Task 1.3.py
@@ -0,0 +1,9 @@
+with open("C:\Gleb\Лабы\Алгосы\Lab-0\input.txt","w") as k:
+    a,b = map(int, input().split())
+    k.write(str(a) + " " + str(b))
+with open("input.txt", "r") as f:
+    string = f.read().split()
+    line = [int(x) for x in string]
+    res = line[0] + line[1]
+with open("C:\Gleb\Лабы\Алгосы\Lab-0\output.txt", "w") as g:
+    g.write(str(res))
