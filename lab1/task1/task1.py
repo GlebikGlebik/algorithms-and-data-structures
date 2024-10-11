b4a9248b4e43b4b0acaab1c5b880370448ab22f5
Gleb <Glebsd22@gmail.com>
2024-10-11 16:19:03 +0300

    task 1 update

--- lab1/task1/task1.py
+++ lab1/task1/task1.py
@@ -12,6 +12,7 @@ start = time.perf_counter()
 with open ("input.txt", "r") as f:
     n = int(f.readline())
     a = f.readline().split()
+    a = [int(x) for x in a]
     for i in range(1, n, 1):
         for j in range (0, i, 1):
             if a[i] < a[i - 1]:
@@ -19,14 +20,13 @@ with open ("input.txt", "r") as f:
                     p = a[j]
                     a[j] = a[i]
                     a[i] = p
+        print(a)
 
 end = time.perf_counter()
 
 with open ("output.txt", "w") as f:
+    a = [str(x) for x in a ]
     f.write(" ".join(a))
 
 print(end - start)
-
-
-
 
