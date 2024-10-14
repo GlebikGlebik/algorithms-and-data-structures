6fff85592f00d44a38510ac3abd25441e3105c2e
Gleb <Glebsd22@gmail.com>
2024-10-14 15:57:11 +0300

    task 4 fixed

--- lab1/task4/task4.py
+++ lab1/task4/task4.py
@@ -3,32 +3,32 @@ import tracemalloc
 
 tracemalloc.start()
 
-with open ("input.txt", "w") as f:
+with open("input.txt", "w") as f:
     a = input().split()
-    n = input()
+    n = input().strip()
     f.write(" ".join(a))
     f.write("\n")
     f.write(n)
 
 start = time.perf_counter()
 
-with open ("input.txt", "r") as f:
+with open("input.txt", "r") as f:
     a = f.readline().split()
-    n = f.readline()
+    n = f.readline().strip()
     k = 0
     b = []
-    for i in range(0, len(a), 1):
+    for i in range(len(a)):
         if a[i] == n:
             k += 1
             b.append(str(i))
 
 end = time.perf_counter()
 
-with open ("output.txt", "w") as f:
+with open("output.txt", "w") as f:
     if k == 0:
         f.write("-1")
     elif k == 1:
-        f.write(str(a[0]))
+        f.write(b[0])
     elif k > 1:
         f.write(str(k))
         f.write("\n")
@@ -38,6 +38,3 @@ print("Время работы: ", end - start, "секунд")
 current, peak = tracemalloc.get_traced_memory()
 print(f"Пиковая память: {peak / 2**20:.2f} MB")
 tracemalloc.stop()
-
-
-
