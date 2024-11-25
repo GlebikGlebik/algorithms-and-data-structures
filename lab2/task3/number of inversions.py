b2f710a8afd231526c90c8b5fdd8f0aa94d0d862
Gleb <Glebsd22@gmail.com>
2024-11-25 18:45:57 +0300

    task 3 in lab 2 updated

--- lab2/task3/number of inversions.py
+++ lab2/task3/number of inversions.py
@@ -3,7 +3,7 @@ import tracemalloc
 
 tracemalloc.start()
 
-with open ("input.txt", "w") as f:
+with open("input.txt", "w") as f:
     n = input()
     a = input().split()
     f.write(n)
@@ -13,19 +13,11 @@ with open ("input.txt", "w") as f:
 start = time.perf_counter()
 
 def merge_and_count(arr, left, mid, right):
-    # Левый и правый подмассивы
     left_part = arr[left:mid + 1]
     right_part = arr[mid + 1:right + 1]
-
-    # Индексы для итерации
     i = j = 0
-    k = left
     inversions = 0
-
-    # Временный массив для объединения
     temp = []
-
-    # Подсчёт инверсий и слияние массивов
     while i < len(left_part) and j < len(right_part):
         if left_part[i] <= right_part[j]:
             temp.append(left_part[i])
@@ -33,17 +25,14 @@ def merge_and_count(arr, left, mid, right):
         else:
             temp.append(right_part[j])
             j += 1
-            inversions += len(left_part) - i  # Все оставшиеся элементы в левом массиве больше текущего элемента из правого
+            inversions += len(left_part) - i
 
-    # Добавляем оставшиеся элементы
     while i < len(left_part):
         temp.append(left_part[i])
         i += 1
     while j < len(right_part):
         temp.append(right_part[j])
         j += 1
-
-    # Переносим отсортированные элементы в оригинальный массив
     for i, val in enumerate(temp):
         arr[left + i] = val
 
@@ -54,10 +43,8 @@ def merge_sort_and_count(arr, left, right):
     inversions = 0
     if left < right:
         mid = (left + right) // 2
-        # Подсчитываем инверсии в левой и правой части
         inversions += merge_sort_and_count(arr, left, mid)
         inversions += merge_sort_and_count(arr, mid + 1, right)
-        # Подсчитываем инверсии между двумя частями
         inversions += merge_and_count(arr, left, mid, right)
     return inversions
 
@@ -70,4 +57,3 @@ inversions = merge_sort_and_count(arr, 0, n - 1)
 
 with open("output.txt", "w") as f:
     f.write(str(inversions) + "\n")
-
