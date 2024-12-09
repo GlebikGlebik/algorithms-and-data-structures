85f6b3ba763fc1654171d4840fb7613a668fdae6
Gleb <Glebsd22@gmail.com>
2024-12-09 15:58:43 +0300

    any things updated

--- lab2/task1/src/merge_sort.py
+++ lab2/task1/src/merge_sort.py
@@ -3,14 +3,14 @@ import tracemalloc
 
 tracemalloc.start()
 
-"""
+
 with open ("../txtf/input.txt", "w") as f:
     n = input()
     a = input().split()
     f.write(n)
     f.write("\n")
     f.write(" ".join(a))
-"""
+
 
 start = time.perf_counter()
 
