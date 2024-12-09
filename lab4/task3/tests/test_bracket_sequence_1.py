f3986975d2c61dd8406010ac4f01b7bb5c63ccdf
Gleb <Glebsd22@gmail.com>
2024-12-09 10:29:32 +0300

    task3 in lab4 tested

---
+++ lab4/task3/tests/test_bracket_sequence_1.py
@@ -0,0 +1,39 @@
+import unittest
+from lab4.task3.src.bracket_sequence_1 import *
+
+import unittest
+
+class TesttestBracketSequence1(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_input(self):
+        self.path = "../txtf/test_input.txt"
+
+    def test_single_bracket(self):
+        self.assertEqual(right_pos("("), "NO")
+        self.assertEqual(right_pos(")"), "NO")
+        self.assertEqual(right_pos("["), "NO")
+        self.assertEqual(right_pos("]"), "NO")
+
+    def test_invalid_sequences(self):
+        self.assertEqual(right_pos("[)"), "NO")
+        self.assertEqual(right_pos("((("), "NO")
+        self.assertEqual(right_pos("([)]"), "NO")
+        self.assertEqual(right_pos("{[}]"), "NO")
+        self.assertEqual(right_pos("((()))"), "NO")
+        self.assertEqual(right_pos("())"), "NO")
+        self.assertEqual(right_pos("([)[]"), "NO")
+        self.assertEqual(right_pos("[[[[]]]"), "NO")
+        self.assertEqual(right_pos("]]]]"), "NO")
+
+
+    def test_unbalanced_brackets(self):
+        self.assertEqual(right_pos("("), "NO")
+        self.assertEqual(right_pos("("), "NO")
+        self.assertEqual(right_pos("("), "NO")
+        self.assertEqual(right_pos("("), "NO")
+        self.assertEqual(right_pos("("), "NO")
+
+if __name__ == "__main__":
+    unittest.main()
