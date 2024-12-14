5917a92a00ae3e06f9bcac1dcd8acfada843a2fa
Gleb <Glebsd22@gmail.com>
2024-12-15 01:11:24 +0300

    task3 in lab4 added utils.py

--- lab4/task3/src/bracket_sequence_1.py
+++ lab4/task3/src/bracket_sequence_1.py
@@ -1,13 +1,13 @@
-import time
-import tracemalloc
+import sys
+import os
+from lab4.utils import read_input, write_output, decorate
 
-tracemalloc.start()
-
-start = time.perf_counter()
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 class Stack:
    def __init__(self):
        self.stack = []
+       self.s = []
 
    def pop(self):
        if len(self.stack) == 0:
@@ -18,30 +18,28 @@ class Stack:
    def push(self, item):
        self.stack.append(item)
 
-def right_pos(A):
-   stc = Stack()
-   if s[0] == ']' or s[0] == ')' or s.count(')') != s.count('(') or s.count(']') != s.count('['):
-       return "NO"
-   for x in s:
-       if x == '(' or x == '[':
-           stc.push(x)
-       else:
-           nr = stc.pop()
-           if (x == ']' and nr == '(') or (x == ')' and nr == '['):
-               return "NO"
-   return "YES"
+   def right_pos(self, s):
+       stc = Stack()
+       if s[0] == ']' or s[0] == ')' or s.count(')') != s.count('(') or s.count(']') != s.count('['):
+           return "NO"
+       for x in s:
+           if x == '(' or x == '[':
+               stc.push(x)
+           else:
+               nr = stc.pop()
+               if (x == ']' and nr == '(') or (x == ')' and nr == '['):
+                   return "NO"
+       return "YES"
 
-with open("../txtf/input.txt", "r") as f:
-    n = int(f.readline())
-    with open("../txtf/output.txt", "w", encoding= 'UTF-8') as g:
-        for i in range(n):
-           s = f.readline().strip()
-           g.write(right_pos(s) + '\n')
+def main():
+    stc = Stack()
+    with open("../txtf/input.txt", "r") as f:
+        n = int(f.readline())
+        with open("../txtf/output.txt", "w", encoding= 'UTF-8') as g:
+            for i in range(n):
+               s = f.readline().strip()
+               g.write(stc.right_pos(s) + '\n')
 
+if __name__ == '__main__':
+    decorate(task=3, task_name='bracket_sequence_1')
 
-end = time.perf_counter()
-
-print("Время работы: ", end - start, "секунд")
-current, peak = tracemalloc.get_traced_memory()
-print(f"Пиковая память: {peak / 2**20:.2f} MB")
-tracemalloc.stop()
