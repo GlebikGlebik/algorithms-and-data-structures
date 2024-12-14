dd6729b322840a86089b4983d67dd8c959490374
Gleb <Glebsd22@gmail.com>
2024-12-15 02:19:54 +0300

    task8.py in lab1 added utils.py

--- lab1/task8/src/task8.py
+++ lab1/task8/src/task8.py
@@ -1,42 +1,29 @@
-from time import *
-import tracemalloc
+import sys
+import os
+from lab1.utils import read_input, write_output, decorate
 
-tracemalloc.start()
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
-with open ("../txtf/input.txt", "w") as f:
-    n = input()
-    a = input().split()
-    f.write(n)
-    f.write("\n")
-    f.write(" ".join(a))
+def main():
+    input_file = read_input(8)
+    n = int(input_file[0])
+    a = list(map(int, input_file[1].split()))
+    res = []
+    for i in range(n - 1):
+        m = i
+        for j in range(i + 1, n):
+            if a[m] > a[j]:
+                m = j
+        if m != i:
+            p = min(m,i) + 1
+            v = max(m,i) + 1
+            res.append(f'Swap elements at indices {str(p)} and {str(v)}.')
+        a[m], a[i] = a[i], a[m]
+    write_output(8, *res, "No more swaps needed.")
 
-start = perf_counter()
+    for q in range(n - 1):
+        if a[q + 1] < a[q]:
+            print("error: invalid sort")
 
-with open ("../txtf/input.txt", "r") as f:
-    with open ("../txtf/output.txt", "w") as g:
-        n = int(f.readline())
-        b = f.readline().split()
-        a = [int(x) for x in b]
-        for i in range(n - 1):
-            m = i
-            for j in range(i + 1, n):
-                if a[m] > a[j]:
-                    m = j
-            if m != i:
-                p = min(m,i) + 1
-                v = max(m,i) + 1
-                g.write("Swap elements at indices " + str(p) + " and " + str(v) + ".")
-                g.write("\n")
-            a[m], a[i] = a[i], a[m]
-        g.write("No more swaps needed.")
-
-end = perf_counter()
-
-for q in range(n - 1):
-    if a[q + 1] < a[q]:
-        print("error: invalid sort")
-
-print("Время работы: ", end - start, "секунд")
-current, peak = tracemalloc.get_traced_memory()
-print(f"Пиковая память: {peak / 2**20:.2f} MB")
-tracemalloc.stop()
+if __name__ == '__main__':
+    decorate(task = 8, task_name= 'task8')
