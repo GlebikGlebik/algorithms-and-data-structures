1d9e3b2bf297ee6de00bf8f2a2a868ec0547201c
Gleb <Glebsd22@gmail.com>
2024-12-15 01:02:13 +0300

    task2 in lab4 added utils.py

--- lab4/task2/src/queue.py
+++ lab4/task2/src/queue.py
@@ -1,38 +1,35 @@
-import time
-import tracemalloc
+import sys
+import os
+from lab4.utils import read_input, write_output, decorate
 
-tracemalloc.start()
-
-start = time.perf_counter()
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 class Queue:
-   def __init__(self):
-       self.queue = []
+    def __init__(self):
+        self.queue = []
 
-   def pop(self):
-       if len(self.queue) == 0:
-           return None
-       res = self.queue.pop(0)
-       return res
+    def pop(self):
+        if len(self.queue) == 0:
+            return None
+        res = self.queue.pop(0)
+        return res
 
-   def push(self, item):
-       self.queue.append(item)
+    def push(self, item):
+        self.queue.append(item)
 
 
-with open("../txtf/input.txt", "r") as f:
-    n = int(f.readline())
-    q = Queue()
-    with open("../txtf/output.txt", "w") as g:
-        for i in range(n):
-           arr = f.readline().split()
-           if arr[0] == '+':
-               q.push(arr[1])
-           elif arr[0] == '-':
-               g.write(str(q.pop()) + "\n")
+def main():
+    with open("../txtf/input.txt", "r") as f:
+        n = int(f.readline())
+        q = Queue()
+        with open("../txtf/output.txt", "w") as g:
+            for i in range(n):
+                arr = f.readline().split()
+                if arr[0] == '+':
+                    q.push(arr[1])
+                elif arr[0] == '-':
+                    g.write(str(q.pop()) + "\n")
 
-end = time.perf_counter()
 
-print("Время работы: ", end - start, "секунд")
-current, peak = tracemalloc.get_traced_memory()
-print(f"Пиковая память: {peak / 2**20:.2f} MB")
-tracemalloc.stop()
+if __name__ == '__main__':
+    decorate(task=2, task_name='queue')
