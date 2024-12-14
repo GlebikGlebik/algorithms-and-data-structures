c8f0fc60790a1f698b1df8cdf4633866cf67fdb2
Gleb <Glebsd22@gmail.com>
2024-12-15 01:23:49 +0300

    task1.py in lab1 added utils.py

--- lab1/task1/src/task1.py
+++ lab1/task1/src/task1.py
@@ -1,21 +1,15 @@
-import time
-import tracemalloc
+import sys
+import os
+from lab1.utils import read_input, write_output, decorate
 
-tracemalloc.start()
-
-with open ("../txtf/input.txt", "w") as f:
-    n = input()
-    a = input().split()
-    f.write(n)
-    f.write("\n")
-    f.write(" ".join(a))
-
-start = time.perf_counter()
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 with open ("../txtf/input.txt", "r") as f:
-    n = int(f.readline())
-    a = f.readline().split()
-    a = [int(x) for x in a]
+    input_file = read_input(task=1)
+    n = int(input_file[0])
+    a = list(map(int, input_file[1].split()))
+
+def main():
     for i in range(1, n, 1):
         for j in range (0, i, 1):
             if a[i] < a[i - 1]:
@@ -24,21 +18,12 @@ with open ("../txtf/input.txt", "r") as f:
                     a[j] = a[i]
                     a[i] = p
 
-for q in range(n - 1):
-    if a[q + 1] < a[q]:
-        print("error: invalid sort")
+    for q in range(n - 1):
+        if a[q + 1] < a[q]:
+            print("error: invalid sort")
 
-end = time.perf_counter()
+    res = [str(i) for i in a]
+    write_output(1, ' '.join(res))
 
-with open ("../txtf/output.txt", "w") as f:
-    a = [str(x) for x in a ]
-    f.write(" ".join(a))
-
-print("Время работы: ", end - start, "секунд")
-
-current, peak = tracemalloc.get_traced_memory()
-
-print(f"Пиковая память: {peak / 2**20:.2f} MB")
-
-tracemalloc.stop()
-
+if __name__ == '__main__':
+    decorate(task=1, task_name='task1')
