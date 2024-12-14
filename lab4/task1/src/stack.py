5abbbe17dfba3c2b12597cf1da5dcc8cd98b3147
Gleb <Glebsd22@gmail.com>
2024-12-15 00:59:07 +0300

    task1 in lab4 added utils.py

--- lab4/task1/src/stack.py
+++ lab4/task1/src/stack.py
@@ -1,38 +1,33 @@
-import time
-import tracemalloc
+import sys
+import os
+from lab4.utils import read_input, write_output, decorate
 
-tracemalloc.start()
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
-start = time.perf_counter()
 
 class Stack:
-   def __init__(self):
-       self.stack = []
+    def __init__(self):
+        self.stack = []
 
-   def pop(self):
-       removed = self.stack.pop()
-       return removed
+    def pop(self):
+        removed = self.stack.pop()
+        return removed
 
-   def push(self, item):
-       self.stack.append(item)
-
-
-with open("../txtf/input.txt", "r") as f:
-    m = int(f.readline())
-    with open("../txtf/output.txt", "w") as g:
-        stc = Stack()
-        for i in range(m):
-           arr = f.readline().split()
-           if arr[0] == '+':
-               stc.push(arr[1])
-           elif arr[0] == '-':
-               g.write(str(stc.pop()) + "\n")
-
-end = time.perf_counter()
+    def push(self, item):
+        self.stack.append(item)
 
 
-print("Время работы: ", end - start, "секунд")
-current, peak = tracemalloc.get_traced_memory()
-print(f"Пиковая память: {peak / 2**20:.2f} MB")
-tracemalloc.stop()
+def main():
+    with open("../txtf/input.txt", "r") as f:
+        m = int(f.readline())
+        with open("../txtf/output.txt", "w") as g:
+            stc = Stack()
+            for i in range(m):
+                arr = f.readline().split()
+                if arr[0] == '+':
+                    stc.push(arr[1])
+                elif arr[0] == '-':
+                    g.write(str(stc.pop()) + "\n")
 
+if __name__ == "__main__":
+    decorate(task=1, task_name="stack")
