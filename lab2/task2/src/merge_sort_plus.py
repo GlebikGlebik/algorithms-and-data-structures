93353691a04423867d0c212e7ea7b405332a7d19
Gleb <Glebsd22@gmail.com>
2024-12-15 03:40:21 +0300

    in task2 in lab2 added utils

--- lab2/task2/src/merge_sort_plus.py
+++ lab2/task2/src/merge_sort_plus.py
@@ -1,77 +1,62 @@
-import time
-import tracemalloc
+import sys
+import os
 
-tracemalloc.start()
+from lab2.utils import read_input, write_output, decorate
 
-"""with open ("../txtf/input.txt", "w") as f:
-    n = input()
-    a = input().split()
-    f.write(n)
-    f.write("\n")
-    f.write(" ".join(a))
-"""
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
-start = time.perf_counter()
+class MergeSortPlus:
+    def __init__(self):
+        self.indexes = []
 
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
+    def merge_sort(self, arr):
+        if len(arr) <= 1:
+            return arr
 
-    mid = len(arr) // 2
-    left_half = merge_sort(dict(list(arr.items())[:mid]))
-    right_half = merge_sort(dict(list(arr.items())[mid:]))
+        mid = len(arr) // 2
+        left_half = self.merge_sort(dict(list(arr.items())[:mid]))
+        right_half = self.merge_sort(dict(list(arr.items())[mid:]))
 
-    return merge(left_half, right_half)
+        return self.merge(left_half, right_half)
 
-def merge(left, right):
-    sorted_array = []
-    i = j = 0
+    def merge(self, left, right):
+        sorted_array = []
+        i = j = 0
 
-    l1 = min(list(left.keys()))
-    r1 = max(list(right.keys()))
+        l1 = min(list(left.keys()))
+        r1 = max(list(right.keys()))
 
-    while i < len(left) and j < len(right):
-        if list(left.values())[i] < list(right.values())[j]:
+        while i < len(left) and j < len(right):
+            if list(left.values())[i] < list(right.values())[j]:
+                sorted_array.append([list(left.keys())[i], list(left.values())[i]])
+                i += 1
+            else:
+                sorted_array.append([list(right.keys())[j], list(right.values())[j]])
+                j += 1
+
+        while i < len(left):
             sorted_array.append([list(left.keys())[i], list(left.values())[i]])
             i += 1
-        else:
+
+        while j < len(right):
             sorted_array.append([list(right.keys())[j], list(right.values())[j]])
             j += 1
 
-    while i < len(left):
-        sorted_array.append([list(left.keys())[i], list(left.values())[i]])
-        i += 1
+        self.indexes.append(f'{str(l1)} {str(r1)} {str(sorted_array[0][1])} {str(sorted_array[-1][1])}')
+        return dict(sorted_array)
 
-    while j < len(right):
-        sorted_array.append([list(right.keys())[j], list(right.values())[j]])
-        j += 1
+def main():
+    msp = MergeSortPlus()
+    input_file = read_input(2)
+    arr = list(map(int, input_file[1].split()))
+    inp_arr = []
+    for i in range(1, len(arr)+1):
+        inp_arr.append([i, arr[i-1]])
+    dict_arr = dict(inp_arr)
+    sorted_arr = msp.merge_sort(dict_arr)
+    res = [str(x) for x in sorted_arr.values()]
+    msp.indexes.append(" ".join(res))
+    write_output(2, *msp.indexes)
 
-
-    f.write(str(l1) + ' ' + str(r1) + ' ' + str(sorted_array[0][1]) + ' ' + str(sorted_array[-1][1]))
-    f.write("\n")
-    return dict(sorted_array)
-
-with open("../txtf/input.txt", "r") as f:
-    n = int(f.readline())
-    arr1 = f.readline().split()
-    arr = [int(x) for x in arr1]
-    with open("../txtf/output.txt", "w") as f:
-        inp_arr = []
-        for i in range(1, len(arr)+1):
-            inp_arr.append([i, arr[i-1]])
-        dict_arr = dict(inp_arr)
-        sorted_arr = merge_sort(dict_arr)
-        res = sorted_arr.values()
-        res = [str(x) for x in sorted_arr.values()]
-        f.write(" ".join(res))
-
-end = time.perf_counter()
-
-print("Время работы: ", end - start, "секунд")
-current, peak = tracemalloc.get_traced_memory()
-print(f"Пиковая память: {peak / 2**20:.2f} MB")
-tracemalloc.stop()
-
-
-
-
+if __name__ == '__main__':
+    decorate(task = 2, task_name= 'merge_sort_plus')
