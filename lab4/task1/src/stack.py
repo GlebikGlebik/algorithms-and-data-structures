c8cb558b5b20fd8fec919e08906aa4fea7ce2c31
Gleb <Glebsd22@gmail.com>
2024-12-15 04:53:01 +0300

    in task1in lab4 added utils

--- lab4/task1/src/stack.py
+++ lab4/task1/src/stack.py
@@ -4,7 +4,6 @@ from lab4.utils import read_input, write_output, decorate
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
-
 class Stack:
     def __init__(self):
         self.stack = []
@@ -18,16 +17,17 @@ class Stack:
 
 
 def main():
-    with open("../txtf/input.txt", "r") as f:
-        m = int(f.readline())
-        with open("../txtf/output.txt", "w") as g:
-            stc = Stack()
-            for i in range(m):
-                arr = f.readline().split()
-                if arr[0] == '+':
-                    stc.push(arr[1])
-                elif arr[0] == '-':
-                    g.write(str(stc.pop()) + "\n")
+    input_file = read_input(1)
+    m = int(input_file[0])
+    pops = []
+    stc = Stack()
+    for i in input_file:
+        arr = i.split()
+        if arr[0] == '+':
+            stc.push(arr[1])
+        elif arr[0] == '-':
+            pops.append(str(stc.pop()))
 
+    write_output(1, *pops)
 if __name__ == "__main__":
     decorate(task=1, task_name="stack")
