dca671ff10a414b17c0632d512554a400afd4de9
Gleb <Glebsd22@gmail.com>
2024-12-16 16:18:07 +0300

    in task3 in lab4 tested & updated

--- lab4/task3/src/bracket_sequence_1.py
+++ lab4/task3/src/bracket_sequence_1.py
@@ -5,20 +5,21 @@ from lab4.utils import read_input, write_output, decorate
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 class Stack:
-   def __init__(self):
+    def __init__(self):
        self.stack = []
        self.s = []
+       self.input_file = read_input(3)
 
-   def pop(self):
+    def pop(self):
        if len(self.stack) == 0:
            return None
        removed = self.stack.pop()
        return removed
 
-   def push(self, item):
+    def push(self, item):
        self.stack.append(item)
 
-   def right_pos(self, s):
+    def right_pos(self, s):
        stc = Stack()
        if s[0] == ']' or s[0] == ')' or s.count(')') != s.count('(') or s.count(']') != s.count('['):
            return "NO"
@@ -31,14 +32,16 @@ class Stack:
                    return "NO"
        return "YES"
 
+    def result(self):
+        answers = []
+        for i in self.input_file[1:]:
+            answers.append(self.right_pos(i))
+        return answers
+
 def main():
     stc = Stack()
-    input_file = read_input(3)
-    answers = []
-    for i in input_file[1:]:
-       answers.append(stc.right_pos(i))
-
-    write_output(3, *answers)
+    res = stc.result()
+    write_output(3, *res)
 
 if __name__ == '__main__':
     decorate(task=3, task_name='bracket_sequence_1')
--- lab4/task3/tests/test_bracket_sequence_1.py
+++ lab4/task3/tests/test_bracket_sequence_1.py
@@ -1,36 +1,88 @@
 import unittest
+from lab4.task3.src.bracket_sequence_1 import *
+from lab4.utils import read_input
 
-class TesttestBracketSequence1(unittest.TestCase):
+
+class TestStack(unittest.TestCase):
     def setUp(self):
         self.stack = Stack()
 
-    def test_input(self):
-        self.path = "../txtf/test_input.txt"
+    def test_case_input(self):
+        # given
+        self.stack.input_file = read_input(3)
+        expected_result = ['YES', 'YES','NO','NO','NO'] # Пример ожидаемой высоты дерева
 
-    def test_single_bracket(self):
-        self.assertEqual(right_pos("("), "NO")
-        self.assertEqual(right_pos(")"), "NO")
-        self.assertEqual(right_pos("["), "NO")
-        self.assertEqual(right_pos("]"), "NO")
+        # when
+        result = self.stack.result()
 
-    def test_invalid_sequences(self):
-        self.assertEqual(right_pos("[)"), "NO")
-        self.assertEqual(right_pos("((("), "NO")
-        self.assertEqual(right_pos("([)]"), "NO")
-        self.assertEqual(right_pos("{[}]"), "NO")
-        self.assertEqual(right_pos("((()))"), "NO")
-        self.assertEqual(right_pos("())"), "NO")
-        self.assertEqual(right_pos("([)[]"), "NO")
-        self.assertEqual(right_pos("[[[[]]]"), "NO")
-        self.assertEqual(right_pos("]]]]"), "NO")
+        # then
+        self.assertEqual(expected_result, result)
 
+    def test_case_1(self):
+        # given
+        self.stack.input_file = ['1', '[]']
+        expected_result = ['YES'] # Пример ожидаемой высоты дерева
 
-    def test_unbalanced_brackets(self):
-        self.assertEqual(right_pos("("), "NO")
-        self.assertEqual(right_pos("("), "NO")
-        self.assertEqual(right_pos("("), "NO")
-        self.assertEqual(right_pos("("), "NO")
-        self.assertEqual(right_pos("("), "NO")
+        # when
+        result = self.stack.result()
 
-if __name__ == "__main__":
+        # then
+        self.assertEqual(expected_result, result)
+
+    def test_case_2(self):
+        # given
+        self.stack.input_file = ['10', '[]', '[()]', '[(])', '(()', '())', '[]()', '[[]]', '((()))', '(([[]]))', '([)]']
+        expected_result = ['YES', 'YES', 'NO', 'NO', 'NO', 'YES', 'YES', 'YES', 'YES', 'NO']  # Ожидаемые результаты для каждой строки
+
+        # when
+        result = self.stack.result()
+
+        # then
+        self.assertEqual(expected_result, result)
+
+    def test_case_3(self):
+        # given
+        self.stack.input_file = ['10', '[]', '[()]', '[(])', '(()', '())', '[]()', '[[]]', '((()))', '(([[]]))', '([)]']
+        expected_result = ['YES', 'YES', 'NO', 'NO', 'NO', 'YES', 'YES', 'YES', 'YES', 'NO']  # Ожидаемые результаты для каждой строки
+
+        # when
+        result = self.stack.result()
+
+        # then
+        self.assertEqual(expected_result, result)
+
+    def test_case_4(self):
+        # given
+        self.stack.input_file = ['5', '((()))', '(()())', '(()', '()((', '[[[]]]']
+        expected_result = ['YES', 'YES', 'NO', 'NO', 'YES']
+
+        # when
+        result = self.stack.result()
+
+        # then
+        self.assertEqual(expected_result, result)
+
+    def test_case_5(self):
+        # given
+        self.stack.input_file = ['6', '([{}])', '[(])', '{[()]}', '((({}))', '([]{})', '([)]']
+        expected_result = ['YES', 'NO', 'YES', 'NO', 'YES', 'NO']  # Ожидаемые результаты для каждой строки
+
+        # when
+        result = self.stack.result()
+
+        # then
+        self.assertEqual(expected_result, result)
+
+    def test_case_6(self):
+        # given
+        self.stack.input_file = ['8', '[]()', '([])', '[()]', '(([]))', '((()))', '((]', '(()))', '([)]']
+        expected_result = ['YES', 'YES', 'YES', 'YES', 'YES', 'NO', 'NO', 'NO']  # Ожидаемые результаты для каждой строки
+
+        # when
+        result = self.stack.result()
+
+        # then
+        self.assertEqual(expected_result, result)
+
+if __name__ == '__main__':
     unittest.main()
