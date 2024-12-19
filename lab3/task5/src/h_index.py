61a277b787971c4cbda4aed23d98a3b22c4a8bdb
Gleb <Glebsd22@gmail.com>
2024-12-19 23:03:32 +0300

    task5 in lab3 tests update

--- lab3/task5/src/h_index.py
+++ lab3/task5/src/h_index.py
@@ -5,21 +5,24 @@ from lab3.utils import read_input, write_output, decorate
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
+
 def h_index(array):
-    h_indexes = []
+    array.sort(reverse=True)
+
+    h = 0
     for i in range(len(array)):
-        count = 0
-        for j in range(1, len(array)):
-            if array[i] >= array[j]:
-                count += 1
-        h_indexes.append(count)
-    return max(h_indexes) - 1
+        if array[i] >= i + 1:  # Проверяем, сколько статей имеют хотя бы i+1 цитирований
+            h = i + 1
+        else:
+            break
+    return h
+
 
 def main():
     input_file = read_input(5)
-    array = list(map(int,input_file[0].split(',')))
+    array = list(map(int, input_file[0].split(',')))
     res = h_index(array)
     write_output(5, str(res))
 
 if __name__ == '__main__':
-    decorate(task = 5, task_name= 'h_index')
+    decorate(task=5, task_name='h_index')
--- lab3/task5/tests/test_h-index.py
+++ lab3/task5/tests/test_h-index.py
@@ -1,27 +1,120 @@
 import unittest
+
 from lab3.task5.src.h_index import *
+from lab3.utils import read_input
 
 class TestHIndex(unittest.TestCase):
 
-    def test_sorted_array(self):
+    def test_case_input(self):
+        # given
+        input_file = read_input(5)
+        array = list(map(int, input_file[0].split(',')))
+        expected_result = 3
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_0(self):
+        # given
+        array = [1, 3, 1]
+        expected_result = 1
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_empty_array(self):
+        # given
+        array = []
+        expected_result = 0  # H-индекс для пустого массива
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_all_zeros(self):
+        # given
+        array = [0, 0, 0, 0]
+        expected_result = 0  # Все нули, H-индекс равен 0
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_single_element(self):
+        # given
+        array = [5]
+        expected_result = 1  # Один элемент, который больше 0
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_multiple_identical_elements(self):
+        # given
+        array = [3, 3, 3, 3]
+        expected_result = 3  # Все элементы одинаковые
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_large_h_index(self):
+        # given
+        array = [10, 8, 5, 4, 3]
+        expected_result = 4  # H-индекс равен 4
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_h_index_equals_length(self):
+        # given
         array = [1, 2, 3, 4, 5]
-        expected = 3
-        self.assertEqual(h_index(array), expected)
+        expected_result = 3  # H-индекс равен 3, так как 3 статьи имеют 3 или более цитирований
 
-    def test_reverse_sorted_array(self):
-        array = [5, 4, 3, 2, 1]
-        expected = 3
-        self.assertEqual(h_index(array), expected)
+        # when
+        res = h_index(array)
 
-    def test_random_array(self):
-        array = [3, 0, 6, 1, 5]
-        expected = 3
-        self.assertEqual(h_index(array), expected)
+        # then
+        self.assertEqual(expected_result, res)
 
-    def test_array_with_duplicates(self):
-        array = [3, 3, 3, 3, 3]
-        expected = 3
-        self.assertEqual(h_index(array), expected)
+    def test_case_descending_order(self):
+        # given
+        array = [6, 5, 3, 1, 0]
+        expected_result = 3  # H-индекс равен 3
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_large_array(self):
+        # given
+        array = list(range(1, 1001))  # Массив от 1 до 1000
+        expected_result = 500  # H-индекс равен 500
+
+        # when
+        res = h_index(array)
+
+        # then
+        self.assertEqual(expected_result, res)
 
 if __name__ == '__main__':
     unittest.main()
