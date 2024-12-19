6cf28221a59614d7e64cbcf999590b692a464d2c
Gleb <Glebsd22@gmail.com>
2024-12-19 22:26:58 +0300

    task1 in lab3 tests update

--- lab3/task1/tests/test_random_quick_sort.py
+++ lab3/task1/tests/test_random_quick_sort.py
@@ -1,37 +1,123 @@
 import unittest
+from array import array
+
+from lab3.task1.src.random_quick_sort import *
+from lab3.utils import read_input
 
 
-class TestQuickSort(unittest.TestCase):
+class TestRandomQuickSort(unittest.TestCase):
 
-    def test_sorted_array(self):
-        array = [1, 2, 3, 4, 5]
-        expected = [1, 2, 3, 4, 5]
-        self.assertEqual(quick_sort(array), expected)
+    def test_case_input(self):
+        # given
+        input_file = read_input(1)
+        array =  list(map(int,input_file[1].split()))
+        expected_result = [1, 2, 3, 4, 5]
 
-    def test_reverse_sorted_array(self):
-        array = [5, 4, 3, 2, 1]
-        expected = [1, 2, 3, 4, 5]
-        self.assertEqual(quick_sort(array), expected)
+        # when
+        res = quick_sort(array)
 
-    def test_random_array(self):
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_0(self):
+        # given
         array = [3, 6, 2, 8, 1, 5]
-        expected = sorted(array)  # Сравниваем с встроенной сортировкой
-        self.assertEqual(quick_sort(array), expected)
+        expected_result = sorted(array)
 
-    def test_empty_array(self):
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_1(self):
+        # given
         array = []
-        expected = []
-        self.assertEqual(quick_sort(array), expected)
+        expected_result = []
 
-    def test_single_element_array(self):
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_2(self):
+        # given
         array = [42]
-        expected = [42]
-        self.assertEqual(quick_sort(array), expected)
+        expected_result = [42]
 
-    def test_array_with_duplicates(self):
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_3(self):
+        # given
         array = [3, 1, 2, 3, 3, 1]
-        expected = sorted(array)  # Сравниваем с встроенной сортировкой
-        self.assertEqual(quick_sort(array), expected)
+        expected_result = sorted(array)
+
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_negative_numbers(self):
+        # given
+        array = [-5, -1, -15, 0, 3, 2]
+        expected_result = sorted(array)
+
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_duplicates(self):
+        # given
+        array = [5, 3, 5, 2, 5, 1]
+        expected_result = sorted(array)
+
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_sorted_array(self):
+        # given
+        array = [1, 2, 3, 4, 5]
+        expected_result = [1, 2, 3, 4, 5]
+
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_large_numbers(self):
+        # given
+        array = [1000000, 500000, 10000000, 0, -1000000]
+        expected_result = sorted(array)
+
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
+    def test_case_large_array(self):
+        # given
+        array = list(range(1000, 0, -1))  # массив от 1000 до 1
+        expected_result = list(range(1, 1001))  # ожидаемый отсортированный массив от 1 до 1000
+
+        # when
+        res = quick_sort(array)
+
+        # then
+        self.assertEqual(expected_result, res)
+
 
 if __name__ == '__main__':
     unittest.main()
